50c44f3c1146f9e8ad8902e7f28b5fbdfbf26f0b
Bruno <brunocesar.oc@hotmail.com>
2019-08-03 19:38:05 -0300

    Salvando e buscando categorias e Adicionando validação para objetos mal escritos no lado cliente com jackson.deserialization

--- lancamentos-api/src/main/java/com/example/lancamentoapi/controller/CategoriaController.java
+++ lancamentos-api/src/main/java/com/example/lancamentoapi/controller/CategoriaController.java
@@ -1,25 +1,54 @@
 package com.example.lancamentoapi.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.lancamentoapi.model.Categoria;
 import com.example.lancamentoapi.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@GetMapping
-	public List<Categoria> listar(){
-		
-		return categoriaRepository.findAll();
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar() {
+
+        return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria, HttpServletResponse response) {
+
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(categoriaSalva.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        /*Retornando objeto criado para o cliente, facilita o desenvolvimento da parte cliente*/
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPeloId(@PathVariable Long id){
+
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+
+        if (categoria.isPresent()){
+            return ResponseEntity.ok(categoria.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
