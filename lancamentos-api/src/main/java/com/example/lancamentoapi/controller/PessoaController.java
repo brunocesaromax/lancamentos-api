987915f6ce91ebd016fe668b7af9fd944533911b
Bruno <brunocesar.oc@hotmail.com>
2019-08-10 13:09:53 -0300

    Deleção de pessoas e criação de exceção personalizada para a deleção

--- lancamentos-api/src/main/java/com/example/lancamentoapi/controller/PessoaController.java
+++ lancamentos-api/src/main/java/com/example/lancamentoapi/controller/PessoaController.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.lancamentoapi.event.RecursoCriadoEvent;
@@ -58,4 +60,12 @@ public class PessoaController {
 
 		return ResponseEntity.notFound().build();
 	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT) // Sucesso porém sem conteúdo
+	public void remover(@PathVariable Long id) {
+		pessoaRepository.deleteById(id);
+	}
+	
+	
 }
--- lancamentos-api/src/main/java/com/example/lancamentoapi/exceptionHandler/LancamentosExceptionHandler.java
+++ lancamentos-api/src/main/java/com/example/lancamentoapi/exceptionHandler/LancamentosExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -43,6 +46,19 @@ public class LancamentosExceptionHandler extends ResponseEntityExceptionHandler 
 
 		List<Erro> erros = getListaErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+//	Tratando exceção do tipo abaixo
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		
+		String msgUsuario = messageSorce.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String msgDev = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(msgUsuario, msgDev));
+		
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		
 	}
 
 	private List<Erro> getListaErros(BindingResult bindingResult) {
