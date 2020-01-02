c07d05874874c4477cd28efeb9543b7a420120b7
Bruno <brunocesar.oc@hotmail.com>
2020-01-02 11:32:31 -0300

    10.5 Adicionando uma tabela de dados

--- lancamentos-ui/src/app/app.component.ts
+++ lancamentos-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lancamentos-ui';
+  launchs = [
+    {
+      type: 'EXPENSE', description: 'Compra de arroz', dueDate: '30/06/2019',
+      payday: '30/07/2019', value: 50.55, person: 'Raimundo'
+    },
+    {
+      type: 'RECIPE', description: 'Receita da boa', dueDate: '02/01/2020',
+      payday: '30/07/2020', value: 121.62, person: 'Lisp'
+    },
+    {
+      type: 'EXPENSE', description: 'Compra de suco', dueDate: '03/05/2020',
+      payday: '30/07/2020', value: 68.99, person: 'Marcos'
+    }
+  ];
+
+  headers = ['Pessoa', 'Descrição', 'Vencimento', 'Pagamento', 'Valor'];
 }
