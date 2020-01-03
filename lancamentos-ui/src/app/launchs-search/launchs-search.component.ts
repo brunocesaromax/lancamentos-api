2d66873f7594b1ddc0c7828eec74a5598cfb13e7
Bruno <brunocesar.oc@hotmail.com>
2020-01-03 19:32:55 -0300

    11.8 Desafio usando pipes

--- lancamentos-ui/src/app/launchs-search/launchs-search.component.ts
+++ lancamentos-ui/src/app/launchs-search/launchs-search.component.ts
@@ -9,16 +9,16 @@ export class LaunchsSearchComponent implements OnInit {
 
   launchs = [
     {
-      type: 'EXPENSE', description: 'Compra de arroz', dueDate: '30/06/2019',
-      payday: '30/07/2019', value: 50.55, person: 'Raimundo'
+      type: 'EXPENSE', description: 'Compra de arroz', dueDate: new Date(2019, 6, 30),
+      payday: new Date(2019, 7, 30), value: 50.55, person: 'Raimundo'
     },
     {
-      type: 'RECIPE', description: 'Receita da boa', dueDate: '02/01/2020',
-      payday: '30/07/2020', value: 121.62, person: 'Lisp'
+      type: 'RECIPE', description: 'Receita da boa', dueDate: new Date(2020, 1, 2),
+      payday: new Date(2020, 7, 30), value: 121.62, person: 'Lisp'
     },
     {
-      type: 'EXPENSE', description: 'Compra de suco', dueDate: '03/05/2020',
-      payday: '30/07/2020', value: 68.99, person: 'Marcos'
+      type: 'EXPENSE', description: 'Compra de suco', dueDate: new Date(2020, 5, 3),
+      payday: new Date(2020, 9, 30), value: 68.99, person: 'Marcos'
     }
   ];
 
