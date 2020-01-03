a933276fe67f05f1bcf5f9704f76eca9f12f26fb
Bruno <brunocesar.oc@hotmail.com>
2020-01-03 18:54:59 -0300

    11.2 Respondendo a eventos do hospedeiro com @HostListener

--- lancamentos-ui/src/app/colored-field.directive.ts
+++ lancamentos-ui/src/app/colored-field.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   // Seletor de atributo, poderia ser também apenas input
@@ -9,9 +9,15 @@ export class ColoredFieldDirective {
   constructor(
     private elementRef: ElementRef, // Dá acesso ao elemento hospedeiro no DOM
     private renderer: Renderer2, // Abstração na manipulação de elementos na DOM
-  ) {
+  ) { }
+
+  @HostListener('focus') onFocus() {
     this.renderer.setStyle(this.elementRef.nativeElement,
       'backgroundColor', 'green');
   }
 
+  @HostListener('blur') withoutFocus() {
+    this.renderer.setStyle(this.elementRef.nativeElement,
+      'backgroundColor', 'transparent');
+  }
 }
