a584b2391f69ef8a876096d1528c6ed306104814
Bruno <bruno.cesar@cilia.com.br>
2020-12-29 15:34:07 -0300

    Deploy

--- lancamentos-ui/src/environments/environment.prod.ts
+++ lancamentos-ui/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 // Repete propriedades do environment default, mas com valores diferentes
 export const environment = {
   production: true,
-  // apiUrl: 'https://launchs-api.herokuapp.com'
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://launchs-api.herokuapp.com'
 };
